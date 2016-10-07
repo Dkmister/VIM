070d6ef4663fbbe982e8fbe694b053f9a4feeeba
dkmisterv <vilmar.neto@gmail.com>
2016-10-07 13:11:52 -0300

    Added canny effect, sobelx and sobely

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    canny = cv2.Canny(frame,100,200)
+
+    sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
+
+    sobely = cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    cv2.imshow('frame2',frame)
+    cv2.imshow('gray',gray)
+    cv2.imshow('rgb',frame)
+    cv2.imshow('canny',canny)
+    cv2.imshow('sobelx',sobelx)
+    cv2.imshow('sobely',sobely)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
